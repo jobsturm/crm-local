ba85d9a1255b18f16681c7f13c655d995fedb346
jobsturm <jobsturm@gmail.com>
2026-07-24 19:02:11 +0200

    🐛 Quit app fully when window closes on all platforms

--- packages/frontend/electron/main.ts
+++ packages/frontend/electron/main.ts
@@ -263,9 +263,7 @@ app.whenReady().then(async () => {
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  app.quit();
 });
 
 app.on('will-quit', () => {
